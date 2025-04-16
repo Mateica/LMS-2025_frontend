60e3805e741c3a9d88755b330934ed50f75f474c
ivankosic <ivankosic55@gmail.com>
2025-04-16 14:00:47 +0200

    Dodata polaganja za studenta na godini

--- src/app/model/student-on-year.ts
+++ src/app/model/student-on-year.ts
@@ -1,11 +1,13 @@
+import { Examination } from "./examination";
 import { Student } from "./student"
 import { YearOfStudy } from "./year-of-study"
 
 export interface StudentOnYear {
     id : number;
-    dateOfApplication : Date;
-    student : Student;
-    yearOfStudy : YearOfStudy;
-    active : boolean;
+	dateOfApplication : Date;
+	student : Student;
+	yearOfStudy : YearOfStudy;
+	examinations : Examination[];
+	active : boolean;
 
 }
