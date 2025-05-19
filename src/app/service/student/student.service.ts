c7cd25cba86e51d5daa503c65ef36392dc384d55
ivankosic <ivankosic55@gmail.com>
2025-05-19 19:30:36 +0200

    Dodata metoda za pretragu u servis za studente

--- src/app/service/student/student.service.ts
+++ src/app/service/student/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { GenericService } from '../base/generic.service';
 import { Student } from '../../model/student';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,23 @@ import { HttpClient } from '@angular/common/http';
 export class StudentService extends GenericService<Student>{
   constructor(http : HttpClient) {
     super(http, "/students");
+  }
+
+  searchStudents(params : any){
+    if(params === undefined){
+      return super.getAll();
+    }else{
+      return super.getAll().pipe(
+        map(r =>{
+          return r.filter(s=>{
+            let result = true;
+            if(s["id"] && params["id"]){
+              result &&= s["id"] && params["id"];
+            }
+
+            return result;
+          });
+        }));
+    }
   }
 }
