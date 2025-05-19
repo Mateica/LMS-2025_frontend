d52a9373b924719927ea95882483b9a8188b5591
ivankosic <ivankosic55@gmail.com>
2025-05-19 21:56:04 +0200

    Dodata metoda za validaciju uloga u servis za autentifikaciju

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,11 +4,14 @@ import { environment } from '../environment';
 import { Observable, tap } from 'rxjs';
 import { LoginResponse, SignupCredentials, UserCredentials } from '../model/user-credentials';
 import { RegisteredUser } from '../model/registered-user';
+import { Role } from '../model/role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user : RegisteredUser | null = null;
+  token : string | null = null;
 
   constructor(private http : HttpClient) { }
 
@@ -19,15 +22,40 @@ export class AuthService {
   login(credentials : UserCredentials) : Observable<LoginResponse>{
     return this.http.post<LoginResponse>(`${environment.baseUrl}/auth/login`, credentials)
       .pipe(tap((result) => {
+        this.token = result["token"];
+        this.user = JSON.parse(atob(result["token"].split(".")[1]));
         localStorage.setItem('authUser', JSON.stringify(result));
       }));
   }
 
   logout() {
+    this.user = null;
+    this.token = null;
     localStorage.clear();
   }
 
   isLoggedIn() {
-    return localStorage.getItem('authUser') !== null;
-  }
+    const authData = localStorage.getItem('authUser');
+    if (!authData) return false;
+
+    const token = JSON.parse(authData).token;
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const now = Math.floor(Date.now() / 1000);
+
+    return payload.exp > now;
+    }
+
+    validateRoles(roles : Role[]){
+      if(this.user){
+        let userRoles = new Set(this.user.roles);
+
+        for(let r of roles){
+          if(userRoles.has(r)){
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }
 }
